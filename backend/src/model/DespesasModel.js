5a2de5565f46cdec322b4908509701da8852c7af
julia <mariajuliaamaraldasilva@gmail.com>
2025-08-16 10:07:19 -0300

    SUBINDO BANCOS / DESPESAS / CATEGORIAS / RECEITAS

---
+++ backend/src/model/DespesasModel.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require('sequelize')
+const Conexao = require('../config/conexao.js')
+
+const DespesasModel = Conexao.define(
+    "DespesasModel", 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        valor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+        },
+        
+        categoria_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                key: 'id',
+                model: CategoriaModel
+            }
+        },
+
+        data_vencimento: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            
+        },
+
+       banco_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                key: 'id',
+                model: BancoModel
+            }
+        },
+        cartao_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                key: 'id',
+                model: CartaoModel
+            }
+        },
+    }, 
+    {
+        tableName: 'despesas'
+    }
+)
+
+module.exports = DespesasModel;
