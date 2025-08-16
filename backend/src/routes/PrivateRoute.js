9b27ece6c069c205f3647d47f3ad44a402542984
Júlio <julio16brito@gmail.com>
2025-08-16 11:06:03 -0300

    Criação de Route cartões

--- backend/src/routes/PrivateRoute.js
+++ backend/src/routes/PrivateRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const UsuariosRoute = require('./UsuariosRoute')
+const CartaoRoute = require('./CartaoRoute')
 const AutenticacaoMiddleware = require('../middleware/AutenticacaoMiddleware')
 
 const PrivateRoute = express.Router()
 PrivateRoute.use(AutenticacaoMiddleware)
 PrivateRoute.use(UsuariosRoute)
+PrivateRoute.use(CartaoRoute)
 
 module.exports = PrivateRoute
