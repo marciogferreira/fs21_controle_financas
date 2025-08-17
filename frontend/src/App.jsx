fb7763aae7562fbad3dad08630f0abe50a32c155
Levi Duarte Lima <duartelevi1@gmail.com>
2025-08-17 00:02:28 -0300

    frontend Javascript

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
